9580ef714e6424a4c627281241d42112a7e72f8a
ldoornenbal <31867982+ldoornenbal@users.noreply.github.com>
2017-09-14 18:22:21 +0200

    Opdrachten les 4 erin verwerkt

--- Week1/Les2/pe2_3.py
+++ Week1/Les2/pe2_3.py
@@ -1,5 +1,5 @@
-vraag1 = 0 == 1 == 2
-vraag2 = 2 + 3 == 4 + 5 == 7
+vraag1 = 0 == (1 == 2)
+vraag2 = 2 + (3 == 4) + 5 == 7
 vraag3 = (1 < -1) == (3 > 4)
 print(vraag1)
 print(vraag2)
---
+++ Week2/Les4/functieOefening.py
@@ -0,0 +1,6 @@
+def hello(naam):
+    print("Welcome, " + naam + ", to the world of Python.")
+
+
+naamInput = str(input("Geef naam in: "))
+hello(naamInput)
